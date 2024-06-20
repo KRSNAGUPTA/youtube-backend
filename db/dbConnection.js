8677754a4a57a6013281d2d7cd8867cdded035e8
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-06-20 18:23:42 +0530

    Database setup completed

---
+++ db/dbConnection.js
@@ -0,0 +1,20 @@
+import { DB_NAME } from "../src/constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`
+    //   , {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+  );
+    console.log(`\n MongoDB connected: ${connectionInstance.connection.host}/${connectionInstance.connection.name}`);
+    return connectionInstance;
+  } catch (error) {
+    console.error(`MongoDB connection error:`, error);
+    process.exit(1); 
+  }
+};
+
+export default connectDB;
