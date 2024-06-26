11d5638d259dbfb9ced25958f1c75c39f0f72295
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-06-26 18:27:42 +0530

    utils

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -3,16 +3,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`
-    //   , {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // }
-  );
-    console.log(`\n MongoDB connected: ${connectionInstance.connection.host}/${connectionInstance.connection.name}`);
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`\nMongoDB connected: ${connectionInstance.connection.host}/${connectionInstance.connection.name}`);
     return connectionInstance;
   } catch (error) {
-    console.error(`MongoDB connection error:`, error);
+    console.error('MongoDB connection error:', error);
     process.exit(1); 
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "../db/dbConnection.js";
+
 dotenv.config();
 
-import connectDB from "../db/dbConnection.js";
-connectDB();
+connectDB().then(() => {
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => {
+    console.log(`Server started at port ${port} \nClick on http://localhost:${port}`);
+  });
+});
