f45e9c2722cf5911c021e9bd28ff62c18ed46182
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-07-17 18:38:40 +0530

    added clodinary and multer

---
+++ utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY,  
+    api_secret: process.env.API_SECRET
+});
+
+const uploadCloudinary =async(localPath)=>{
+    try {
+        if (!localPath) {
+            return null            
+        }
+        const response = await cloudinary.uploader.upload(localPath,{
+            resource_type:"auto"
+        })
+        console.log(response.url);
+        console.log("File is uploaded on cloudinary");
+        return response;
+    } catch (error) {
+       fs.unlinkSync(localPath) 
+       return null;
+    }
+}
+export {uploadCloudinary}
