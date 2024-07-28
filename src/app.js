c443a190c11093bf585851b25de8e4b2c4ed8c13
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-07-28 19:31:55 +0530

    fixed cookie parser

--- src/app.js
+++ src/app.js
@@ -12,9 +12,11 @@ app.use(
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // routes
 import userRouter from "../routes/user.route.js";
 app.use("/api/v1/user",userRouter)
+
 
 export { app };
