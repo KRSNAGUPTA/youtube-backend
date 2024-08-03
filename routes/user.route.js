1df54b43c78cad1655a8877791986dc92d671c10
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-08-03 15:57:04 +0530

    added route for refresh token

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
 
 // import controllers
-import { registerUser,loginUser,logoutUser} from "../controllers/user.controller.js"
+import { registerUser,loginUser,logoutUser, refreshAccessToken} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 
@@ -11,7 +11,7 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 router.route("/register").post(
     upload.fields([
         {
-            name : "avatar",
+            name : "avatar",    
             maxCount:1
         },{
             name:"coverImage",
@@ -26,8 +26,10 @@ router.route("/login").post(
     loginUser
 )
 
+//secured routes
 router.route("/logout").post(
     verifyJWT,
     logoutUser
 )
+router.route("/refresh-token").post(refreshAccessToken)
 export default router;
