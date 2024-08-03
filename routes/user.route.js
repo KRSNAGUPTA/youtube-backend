41ff2bcbfcc3912b3c1b2ef7c9a5ae6e48d3e006
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-08-03 22:00:00 +0530

    add route to remove cover img

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,33 +3,45 @@ const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
 
 // import controllers
-import { registerUser,loginUser,logoutUser, refreshAccessToken} from "../controllers/user.controller.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  getUser,
+  updateAvatar,
+  updateCoverImage,
+  updateAccountDetails,
+  removeCoverImage,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-
 
 // use routes
 router.route("/register").post(
-    upload.fields([
-        {
-            name : "avatar",    
-            maxCount:1
-        },{
-            name:"coverImage",
-            maxCount : 1
-        }
-    ]),
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-
-router.route("/login").post(
-    loginUser
-)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(verifyJWT, refreshAccessToken);
+router.route("/update-details").post(verifyJWT, updateAccountDetails);
+router.post("/update-avatar", verifyJWT, upload.single('avatar'), updateAvatar);
+router.route("/update-coverimg").post(
     verifyJWT,
-    logoutUser
-)
-router.route("/refresh-token").post(refreshAccessToken)
+    upload.fields([{ name: "coverImg", maxCount: 1 }], updateCoverImage)
+  );
+router.route("/remove-coverimg").post( verifyJWT, removeCoverImage)
+router.route("/get-user").get(getUser);
 export default router;
