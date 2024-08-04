b7ff0ff990c6d49ae10e489c7a8f3485aef12f98
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-08-04 10:29:48 +0530

    optimized multer middleware

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -5,9 +5,10 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
     }
+    
   })
   
 export const upload = multer({ 
